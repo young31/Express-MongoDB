9dc55a9e5e8ec028419981a0a0b3f3bbe882621f
young31 <migael38317@gmail.com>
2019-11-19 22:07:07 +0900

    test

--- app.js
+++ app.js
@@ -33,19 +33,18 @@ app.use(bodyParser.json())
 const port = process.env.PORT || 5000
 
 // model setting
-const Test = require('./models/test')
-const SimpleFin = require('./models/simplefin')
+// const Test = require('./models/test')
+// const SimpleFin = require('./models/simplefin')
 
 // router setting
 const home = require('./routes/index')
 app.use('/', home)
 
-const test = require('./routes/test')
-  // const Test = require('./models/test')
-app.use('/test', test)
+// const test = require('./routes/test')
+// app.use('/test', test)
 
-const simpleFin = require('./routes/simplefin')
-app.use('/simpleifn', simpleFin)
+// const simpleFin = require('./routes/simplefin')
+// app.use('/simpleifn', simpleFin)
 
 // confirm connection
 const server = app.listen(port, () => console.log('server listen', port))
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router
-  .get('/', function(req, res) {
-    res.send('신한 해커톤 DB Server')
-  })
+router.get('/', function(req, res) {
+  res.send('신한 해커톤 DB Server')
+})
+
 
 module.exports = router
